87c312e8759e13cca663427a94bd3dcea2ef1ad8
Rodrigue Roland CODO <codorol22@gmail.com>
2022-09-25 21:21:55 +0500

    connect local postgres database

--- urshop/settings.py
+++ urshop/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'urshop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'urshop',
+        'USER' : os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'HOST' : os.environ.get('DATABASE_HOST'),
+        'POST' : os.environ.get('DATABASE_PORT'),
     }
 }
 
