09ecc8bd41ba193a772590c7b0011830c9b961a6
Rodrigue Roland CODO <codorol22@gmail.com>
2022-10-06 01:38:42 +0500

    Added a base footer to the app

--- urshop/views.py
+++ urshop/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpRequest,HttpResponse
 
+from products.views import product_list
+
 def home(request: HttpRequest) -> HttpResponse:
 
-    return render(request,template_name='home.html')
+    return product_list(request)
