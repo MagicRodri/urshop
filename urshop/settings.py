63bce2bf86ea23fb5e0f3a46d66cc78f1f7baf3e
Rodrigue Roland CODO <codorol22@gmail.com>
2022-11-12 01:56:44 +0500

    -

--- urshop/settings.py
+++ urshop/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = 'urshop.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'urshop',
+        'NAME': os.environ.get('DATABASE_NAME','urshop'),
         'USER' : os.environ.get('DATABASE_USER'),
         'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
         'HOST' : os.environ.get('DATABASE_HOST'),
